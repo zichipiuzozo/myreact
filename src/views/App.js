bad1efc4cb0e6e10171628bfc493c05df0b81806
Albert Nguyen <tuananhbg19797@gmail.com>
2022-05-21 16:28:41 +0700

    ...

--- src/views/App.js
+++ src/views/App.js
@@ -1,10 +1,14 @@
 import './App.scss';
 import MyComponent from './example/myComponent.js';
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
-      <div><MyComponent /></div>
+      <header className='App-Header'>
+        <img src={logo} className="App-logo" alt="logo" />
+        <div><MyComponent /></div>
+      </header>
     </div>
   );
 }
--- src/views/example/myComponent.js
+++ src/views/example/myComponent.js
@@ -1,24 +1,58 @@
 import React from "react";
 
-class MyComponent extends React.Component{
-    state = {
-        name : 'Tuấn Anh'
-    }
-    handleOnChangeName = (event) => {
-        this.setState({
-            name : event.target.value
-        })
-    }
-    render(){
-        
-        return(
-            <>
-            <input value={this.state.name} type="text"
-            onChange={(event) => this.handleOnChangeName(event)}/>
-            <div>hello component của {this.state.name} </div>
-            </>
-        )
-    }
+class MyComponent extends React.Component {
+  state = {
+    firstName: "",
+    lastName: "",
+  };
+  handleChangeFirstName = (event) => {
+    this.setState({
+      firstName: event.target.value,
+    });
+  };
+  handleChangeLastName = (event) => {
+    this.setState({
+      lastName: event.target.value,
+    });
+  };
+  handleOnclick = (event) => {
+    event.preventDefault();
+    console.log(">>>> check data render:", this.state);
+  };
+
+  render() {
+    console.log("rendering: ", this.state);
+    return (
+      <>
+        <div>
+          <form>
+            <label htmlFor="fname">First name:</label>
+            <br />
+            <input
+              type="text"
+              value={this.state.firstName}
+              onChange={(event) => this.handleChangeFirstName(event)}
+            />
+            <br />
+            <label htmlFor="lname">Last name:</label>
+            <br />
+            <input
+              type="text"
+              value={this.state.lastName}
+              onChange={(event) => this.handleChangeLastName(event)}
+            />
+            <br />
+            <br />
+            <input
+              type="submit"
+              value="submit"
+              onClick={(event) => this.handleOnclick(event)}
+            />
+          </form>
+        </div>
+      </>
+    );
+  }
 }
 
 export default MyComponent;
